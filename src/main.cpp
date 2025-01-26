44660b03712665f8d47e7f2228646495001ab5e0
Jay <jayay98@gmail.com>
2025-01-26 18:19:53 +0800

    [Enhancement] Using cpp-libp2p example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <libp2p/multi/multiaddress.hpp>
 
-int main(void)
-{
-    std::cout << "Hello World!\n";
-    return 0;
+using namespace libp2p;
+
+int main() {
+  auto addr = multi::Multiaddress::create("/ip4/192.168.0.1/tcp/8080");
+  std::cout << "address: " << addr.value().getStringAddress() << std::endl;
+  return 0;
 }
